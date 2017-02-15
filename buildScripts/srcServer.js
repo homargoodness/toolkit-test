36e6b6164549eb37c4c3060c7da2062d7544031b
homargoodness <o.manir@gmail.com>
2017-02-15 22:46:22 +0000

    configured webpack for js files

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,16 +1,24 @@
-let express = require('express');
-let path = require('path');
-let open = require('open');
+import express from 'express'
+import path from 'path'
+import open from 'open'
+import webpack from 'webpack'
+import webpackConfig from '../webpack.config.dev'
 
 let port = 3000;
 let app = express();
+let compiler = webpack(webpackConfig);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+	noInfo: true,
+	publicPath: webpackConfig.output.publicPath
+}));
 
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, '../src/index.html'));	
 });
 
 app.listen(port, function(err) {
-	console.log('express listing on' + port);
+	console.log('express listing on ' + port);
 	if(err) {
 		console.log(err);
 	} else {
